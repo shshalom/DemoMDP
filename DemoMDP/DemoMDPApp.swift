e9f73382a4ca93798f24bc2582e2b38862e1ee32
Shalom Shwaitzer <shalom.shwaitzer@autodesk.com>
2025-11-11 23:46:49 -0600

    Initial Commit

---
+++ DemoMDP/DemoMDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMDPApp.swift
+//  DemoMDP
+//
+//  Created by Shalom Shwaitzer on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoMDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
