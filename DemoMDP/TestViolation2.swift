6bcc0c2c2ddac0d04f1e340360c0349bac5eeb7e
Shalom Shwaitzer <shalom.shwaitzer@autodesk.com>
2025-11-16 23:03:12 -0600

    test violation

---
+++ DemoMDP/TestViolation2.swift
@@ -0,0 +1,24 @@
+//
+//  TestViolation2.swift
+//  DemoMDP
+//
+//  Created by Shalom Shwaitzer on 11/11/25.
+//
+
+import SwiftUI
+
+struct TestViolation2: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TestViolation2()
+}
